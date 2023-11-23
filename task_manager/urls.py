db10436e6f6eae412f400ee951864b6a778ecd78
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-11-23 11:24:08 +0200

    Add task type list template and fix views to task manager

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -10,10 +10,10 @@ from task_manager.views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path("task-types/", TaskTypeListView(), name="task-type-list"),
-    path("positions/", PositionListView(), name="position-list"),
-    path("tasks/", TaskListView(), name="task-list"),
-    path("workers/", WorkerListView(), name="worker-list")
+    path("task-types/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("positions/", PositionListView.as_view(), name="position-list"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
 ]
 
 app_name = "task_manager"
