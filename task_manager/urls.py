1297d23f09cd49fc50f112e23f072827e5ef4969
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-11-23 15:59:40 +0200

    Import views for models to task manager

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -5,13 +5,27 @@ from task_manager.views import (
     TaskTypeListView,
     PositionListView,
     TaskListView,
-    WorkerListView
+    WorkerListView,
+    TaskTypeCreateView,
+    TaskTypeUpdateView,
+    TaskTypeDeleteView,
+    PositionCreateView,
+    PositionUpdateView,
+    PositionDeleteView,
+    TaskDetailView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
+    WorkerDetailView,
+    WorkerPositionUpdateView,
+    WorkerDeleteView
 )
+
 
 urlpatterns = [
     path("", index, name="index"),
     path("task-types/", TaskTypeListView.as_view(), name="task-type-list"),
-    path("task-types/create/", TaskTypeCreatetView.as_view(), name="task-type-create"),
+    path("task-types/create/", TaskTypeCreateView.as_view(), name="task-type-create"),
     path("task-types/<int:pk>/update/", TaskTypeUpdateView.as_view(), name="task-type-update"),
     path("task-types/<int:pk>/delete/", TaskTypeDeleteView.as_view(), name="task-type-delete"),
     path("positions/", PositionListView.as_view(), name="position-list"),
@@ -21,11 +35,11 @@ urlpatterns = [
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
-    path("tasks/<int:pk>/update/", TaskUptadeView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
     path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
-    path("workers/<int:pk>/update/", WorkerUpdateView.as_view(), name="worker-update"),
+    path("workers/<int:pk>/update/", WorkerPositionUpdateView.as_view(), name="worker-update"),
     path("workers/<int:pk>/delete/", WorkerDeleteView.as_view(), name="worker-delete"),
 ]
 
