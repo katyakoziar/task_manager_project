cd6ddfebade22f7be9befb8e87abe3c7e5a6fc67
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-11-23 09:24:53 +0200

    Create view function and path to home page

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,18 @@
+from django.db.models import QuerySet
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task_manager.models import TaskType, Position, Task, Worker
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    context = {
+        "num_task_types": TaskType.objects.count(),
+        "num_positions": Position.objects.count(),
+        "num_tasks": Task.objects.count(),
+        "num_workers": Worker.objects.count()
+    }
+
+    return render(request, "task_manager/index.html", context=context)
