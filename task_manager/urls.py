efc5276bb8b1488a9cdbba4132f32bffe3237b30
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-11-23 10:57:49 +0200

    Add urls for list views to task manager

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
-from task_manager.views import index
+from task_manager.views import (
+    index,
+    TaskTypeListView,
+    PositionListView,
+    TaskListView,
+    WorkerListView
+)
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("task-types/", TaskTypeListView(), name="task-type-list"),
+    path("positions/", PositionListView(), name="position-list"),
+    path("tasks/", TaskListView(), name="task-list"),
+    path("workers/", WorkerListView(), name="worker-list")
 ]
 
 app_name = "task_manager"
