5fc7eb0a1aa58867a7cf7c9718f7d5f001f6a84c
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-11-23 15:26:37 +0200

    Add create, update, delete views for task type model to task manager

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,6 @@
 from django.db.models import QuerySet
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from task_manager.models import TaskType, Position, Task, Worker
@@ -26,19 +27,36 @@ class TaskTypeListView(generic.ListView):
     context_object_name = "task_type_list"
 
 
+class TaskTypeCreateView(generic.CreateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
+class TaskTypeUpdateView(generic.UpdateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
+class TaskTypeDeleteView(generic.DeleteView):
+    model = TaskType
+    success_url = reverse_lazy("task_manager:task-type-list")
+
+
 class TaskListView(generic.ListView):
     model = Task
-    queryset = Task.objects.select_related("task_types")
+    queryset = Task.objects.select_related("task_type")
     paginate_by = 5
 
 
 class PositionListView(generic.ListView):
     model = Position
-    queryset = TaskType.objects.order_by("name")
+    queryset = Position.objects.order_by("name")
     paginate_by = 5
 
 
 class WorkerListView(generic.ListView):
     model = Worker
     paginate_by = 5
-    queryset = Worker.objects.select_related("positions")
+    queryset = Worker.objects.select_related("position")
