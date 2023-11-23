cbba85886b3c66e73ba4ef37af4319c8f9e8150f
Kateryna Koziar <katya.kozyar.333@gmail.com>
2023-11-23 10:48:33 +0200

    Add class-based views for models to task manager

--- task_manager/views.py
+++ task_manager/views.py
@@ -16,3 +16,29 @@ def index(request):
     }
 
     return render(request, "task_manager/index.html", context=context)
+
+
+class TaskTypeListView(generic.ListView):
+    model = TaskType
+    queryset = TaskType.objects.order_by("name")
+    paginate_by = 5
+    template_name = "task_manager/task_type_list.html"
+    context_object_name = "task_type_list"
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.select_related("task_types")
+    paginate_by = 5
+
+
+class PositionListView(generic.ListView):
+    model = Position
+    queryset = TaskType.objects.order_by("name")
+    paginate_by = 5
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    paginate_by = 5
+    queryset = Worker.objects.select_related("positions")
